6bc62582b1ede23012499a4cfb0d9f7f37228268
LarisaLG <lgalisheva@gmail.com>
2022-07-08 09:33:13 +0000

    Initial commit, add the planned structure of the quiz

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+start_game()
+    #display a question with multiple answers
+
+
+check_answer()
+    #check user input
+   
+    
+count_cor_answers()
+    #count correct user answers
+
+show_results()
+    #print results for user
+
+new_game()
+    #ask the user if he wants to play again
